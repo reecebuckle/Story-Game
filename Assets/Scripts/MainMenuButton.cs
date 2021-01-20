18abb398d09b1b9f470e1077db1cb337bc842707
Krigachu <kg4g18@soton.ac.uk>
2021-01-20 23:48:31 +0000

    Updated ending cutscenes
    All cutscenes should run properly.
    All cutscenes now have a button linking to main menu

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour
+{
+    
+    /*
+     * Loads main menu
+     */
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
